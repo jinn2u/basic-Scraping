c2b1da423ac0c9952aebd5923a32d71c9ccf9290
jinn2u <70435257+jinn2u@users.noreply.github.com>
2020-10-09 15:54:06 +0900

    Update 01.py

--- base/01.py
+++ base/01.py
@@ -1,8 +1,13 @@
 # 라이브러리 읽어들이기
 import urllib.request
-# url과 저장경로 지저하기
+
+# url불러들이기
 url = 'http://uta.pw/shodou/img/28/214.png'
+
+# 저장이름 지정하기
 savename = 'test2.png'
+
 # 다운로드
 urllib.request.urlretrieve(url,savename)
+
 print('저장되었습니다----')
